c2031dce0af9a8830b26cc86844a85b2533f9e4c
faisal-zaheervi <faisalszaheer@gmail.com>
2017-07-16 11:01:54 -0700

    update 1101

---
+++ website/README.md
@@ -0,0 +1,25 @@
+<h1> Project BlueBerry - Resource Website </h1>
+<hr>
+<h3> The Team </h3>
+David - SCRUMmaster <br>
+Michelle - Content Editor <br>
+Brian - Lead Designer <br>
+Faisal - Lead Developer
+<br>
+<h2> To-Do List </h2>
+<ul>
+<li>Decide what Home page will look like</li>
+<br>
+<li>5 Technology internships</li>
+<li>5 Business internships</li>
+<li>5 Medical internships</li>
+<br>
+<li>5 Technology Jobs</li>
+<li>5 Business Jobs</li>
+<li>5 Medical Jobs</li>
+<br>
+<li>10 Volunteer opportunities</li>
+<br>
+<li>Layout About page</li>
+<br>
+</ul>
---
+++ website/javascript.js
@@ -0,0 +1,66 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+/*****************
+HOME
+*****************/
+/*****************
+SOURCE CODE FOUND AT https://www.w3schools.com/w3css/w3css_slideshow.asp
+Combined two functions for my preferences and was edited according to my needs
+*****************/
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("buttonNum");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace("_current", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += "_current";
+
+}
+var slideIndex = 0;
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+    showDivs(slideIndex);
+}
