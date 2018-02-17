67ceac15e9bff12a69614695fc6672bff1e5048e
Faisal-ZaheerVI <faisalszaheer@gmail.com>
2018-02-17 12:37:08 -0800

    Create higherorderfunctions.js
    Going over higher order functions w/ Maurice. (SSP)

---
+++ ssp/higherorderfunctions.js
@@ -0,0 +1,52 @@
+var testObj = {
+  programManager : 'Devontae'
+};
+
+// Dot notation, but bracket notation is preferred
+testObj.name = 'MissionBit';
+
+// Adding values using bracket notation
+// Can only use bracket notation with string values
+// testObj.1 = 'one'; => WRONG
+// testObj[1] = 'one'; => RIGHT | Output => '1' : 'one'
+testObj['place'] = 'China Town';
+
+var meeting = 'WhereWeWillBeSaturday';
+
+testObj[meeting] = 'In the office';
+
+// console.log(testObj);
+
+// Intro to higher order functions
+var testArray = [1,2,3,4,5,6,7];
+
+var each = function(collection, someFunction) {
+  // It will take a function and a collection as a parameters
+  // Loop through the collection
+  if (Array.isArray(collection)) {
+    for (var i = 0; i < collection.length; i++) {
+      // Call someFunction on each value (each testArray value)
+      someFunction(collection[i], i);
+    }
+  }
+  else {
+    for (var key in collection) {
+      someFunction(collection[key], key);
+    }
+  }
+};
+
+// each(testArray, function(element, index) {
+//   console.log('The index of => ' + element + ' is ', index);
+// });
+
+// The function in each is an anonymous function
+// each(testObj, function(value, key) {
+//   console.log('The value at => ' + key + ' is => ' + value);
+// })
+
+var sum = function(element) {
+  console.log(element + 100);
+}
+
+each(testArray, sum);
